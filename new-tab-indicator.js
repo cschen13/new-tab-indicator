1b009e398484537fd6972a1401d5973524bc740f
Chris Chen <cschen13@gmail.com>
2021-01-10 21:26:03 -0800

    Improve handling of post-load attribute changes
    This commit adds a MutationObserver to listen for attribute changes
    on <a> elements in case there are changes after page load. Right now
    the observer won't catch all DOM changes that happen (perhaps from a bug
    resulting from my not understanding the order in which page/content
    scripts execute), so for now the workaround is we always wait for 1
    second before querying for the links to instrument.

--- new-tab-indicator.js
+++ new-tab-indicator.js
@@ -2,23 +2,57 @@ const indicator = createIndicator();
 document.body.appendChild(indicator);
 
 document.onmousemove = (event) => {
-  console.log("mouse is moving");
+  // console.log("mouse is moving");
   indicator.style.left = `${event.pageX + 15}px`;
   indicator.style.top = `${event.pageY - 15}px`;
 };
 
-const links = getLinks();
-// console.log(links);
-links.forEach((link) => {
+const instrumentLink = (link) => {
+  console.log("Instrumenting link...");
+
   link.onmouseenter = function (event) {
-    console.log(`X: ${event.pageX}`);
-    console.log(`y: ${event.pageY}`);
+    // console.log(`X: ${event.pageX}`);
+    // console.log(`y: ${event.pageY}`);
     indicator.style.display = "block";
   };
 
   link.onmouseleave = () => {
     indicator.style.display = "none";
   };
+};
+
+const mutationObserver = new window.MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    console.log(mutation);
+    if (mutation.type === "childList") {
+      console.log("new nodes added");
+      mutation.addedNodes.forEach((node) => {
+        if (node.nodeType === 1 && node.nodeName === "A") {
+          console.log(node);
+          const target = node.getAttribute("target");
+          const onclick = node.getAttribute("onclick");
+          if (target === "_blank" || onclick.includes("window.open(")) {
+            instrumentLink(node);
+          }
+        }
+      });
+    } else if (mutation.type === "attributes") {
+      console.log("attributes changed");
+      if (mutation.target.nodeType === 1 && mutation.target.nodeName == "A") {
+        const target = node.getAttribute("target");
+        const onclick = node.getAttribute("onclick");
+        if (target === "_blank" || onclick.includes("window.open(")) {
+          instrumentLink(node);
+        }
+      }
+    }
+  });
+});
+
+mutationObserver.observe(document.body, {
+  childList: true,
+  subtree: true,
+  attributeFilter: ["target", "onclick"],
 });
 
 function createIndicator() {
@@ -32,8 +66,13 @@ function createIndicator() {
   return indicator;
 }
 
+setTimeout(() => getLinks().forEach(instrumentLink), 1000);
+
 function getLinks() {
-  return document.querySelectorAll(
+  const links = document.querySelectorAll(
     'a[target="_blank"], a[onclick*="window.open("]'
   );
+
+  // console.log(links);
+  return links;
 }
